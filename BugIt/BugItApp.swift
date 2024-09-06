72515b72419e2017ee7431bb7a1a03697c449124
Abdulrahman Alaa <abdulrhmanalaa114@gmail.com>
2024-09-06 14:08:47 +0300

    Initial Commit

---
+++ BugIt/BugItApp.swift
@@ -0,0 +1,17 @@
+//
+//  BugItApp.swift
+//  BugIt
+//
+//  Created by Abdulrahman Alaa on 06/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct BugItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
